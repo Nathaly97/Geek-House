cb2e4e9973e6b9afa3a3be1cd31f1896c78b9006
Angie N. Gonzalez <nthalygc1997@gmail.com>
2024-08-05 22:04:03 -0500

    Js

---
+++ Js/login.js
@@ -0,0 +1,11 @@
+
+
+// Vamos a declarar las variables donde se seleccionen los elementos DOM Del html
+
+var formulario_login = document.querySelector(".formulario-login");
+var formulario_registro = document.querySelector(".formulario-registrO");
+var contenedor_login_registrO = document.querySelector(".contenedoR-login-registrO");
+var caja_trasera_login = document.querySelector(".caja-trasera-login");
+var caja_trasera_registro = document.querySelector(".caja-trasera-registrO");
+
+//querySelector es una herramienta fundamental en JavaScript que nos permite seleccionar elementos del DOM (Document Object Model), es decir, de la estructura de nuestro documento HTML. 44:00
